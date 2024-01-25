11a8fe4483f2b2b03136052fe58c5494cc6c49e7
Шарифзода Икбол <iqbolsharifzoda724@gmail.com>
2024-01-26 00:58:38 +0300

    step-2

---
+++ step-2/step-2.8.py
@@ -0,0 +1,29 @@
+class Bag:
+    def __init__(self):
+        self.data = []
+
+    def add(self, x: str):
+        self.data.append(x)
+
+    def addtwice(self, x: str):
+        self.add(x)
+        self.add(x)
+
+    def remove_add(self,x: str):    
+        if x in self.data:
+            self.data.remove(x)
+        else:
+           raise ValueError("Error")
+
+x = Bag()
+value = x.add("Hello World")
+print(*x.data,end=" ")
+x.remove_add(
+    "Hello World"
+)
+print()
+dif = x.addtwice("True")
+print(*x.data,end=" ")
+
+#Hello World
+#True True
