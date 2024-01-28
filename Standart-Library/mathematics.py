d0e7455031daa1b8433af990bd43c8065efe7773
Шарифзода Икбол <iqbolsharifzoda724@gmail.com>
2024-01-28 16:35:15 +0300

    *

---
+++ Standart-Library/mathematics.py
@@ -0,0 +1,5 @@
+import math
+
+print(math.cos(math.pi/4)) # 0.7071067811865476
+
+print(math.log(1024,2)) # 10.0
---
+++ Standart-Library/random_.py
@@ -0,0 +1,12 @@
+
+import random
+
+list_t = ['apple','pear','banana']
+
+print(random.choice(list_t)) # banana
+
+print(random.sample(range(100),10)) # 26, 21, 24, 75, 35, 89, 76, 22, 4, 64
+
+print(random.random()) # 0.13521755649363665
+
+print(random.randrange(10)) # 6
